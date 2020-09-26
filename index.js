fdbb577ec292829080c3153c24146ff9bd1467ed
Carlos Estrada <carlos.estrada@piikaaboo.com>
2020-09-26 18:22:31 -0400

    Update files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const isUnique = require('./chapter01/IsUnique')
+const isUnique = require('./chapter01/1.1/IsUnique')
 
 
 const words = ["abc", "abcc", "xyz"]; // true, false true
 words.forEach((word) => {
-    console.log('IsUnique?', isUnique(word));
-})
+    console.log(`Is ${word} unique? ${isUnique(word)}`);
+});
